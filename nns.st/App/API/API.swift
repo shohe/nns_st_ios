c33b9d33a5bbe5cc9e4b4bce749bda21780371a6
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-10 20:33:56 +0900

    test method for API

--- nns.st/App/API/API.swift
+++ nns.st/App/API/API.swift
@@ -14,5 +14,21 @@ import BrightFutures
 
 class API {
     
+    class func testCodeForAPI() -> Future<AnyObject, NSError> {
+        let promise = Promise<AnyObject, NSError>()
+        let queue = DispatchQueue(label: "testCodeForAPI", attributes: .concurrent)
+        Alamofire.request("https://jsonplaceholder.typicode.com/todos/1").responseJSON(queue: queue) { (response) in
+            switch response.result {
+            case .success(let value):
+                print("json : \(value)")
+                promise.success(value as AnyObject)
+            case .failure(let error):
+                print("error : \(error)")
+                promise.failure(error as NSError)
+            }
+        }
+        return promise.future
+    }
+    
 }
 
