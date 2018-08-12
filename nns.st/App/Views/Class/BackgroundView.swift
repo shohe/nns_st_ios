66520703e7d5f7ec21e42e4c8255c131cf2c182b
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-12 14:13:25 +0900

    add BackgroundViewClass for gradation background color

---
+++ nns.st/App/Views/Class/BackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundView.swift
+//  nns.st
+//
+//  Created by SHOHE on 2018/08/12.
+//  Copyright © 2018 OhtaniShohe. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
