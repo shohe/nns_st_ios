2f4ad21ad3caa917c9539d7a1856b0bfc83c172a
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-16 19:23:04 +0900

    prepare MakeOfferViewController / Storyboard

---
+++ nns.st/App/ViewControllers/Class/MakeOfferViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MakeOfferViewController.swift
+//  nns.st
+//
+//  Created by SHOHE on 2018/08/16.
+//  Copyright © 2018 OhtaniShohe. All rights reserved.
+//
+
+import UIKit
+
+class MakeOfferViewController: UIViewController {
+    
+    static func instantiateViewController() -> UINavigationController {
+        let storyboard = UIStoryboard(name: "Offer", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "MONavigationController") as! UINavigationController
+        return viewController
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.transparentNavigationBar()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
