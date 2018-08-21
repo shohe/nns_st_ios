d4e632408e976d8d34e68b50f3e74d5c08d702fe
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-21 20:27:54 +0900

    add evaluate star function

--- nns.st/App/ViewControllers/Class/ReviewViewController.swift
+++ nns.st/App/ViewControllers/Class/ReviewViewController.swift
@@ -11,7 +11,14 @@ import UIKit
 class ReviewViewController: UIViewController {
     
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var star01: UIButton!
+    @IBOutlet weak var star02: UIButton!
+    @IBOutlet weak var star03: UIButton!
+    @IBOutlet weak var star04: UIButton!
+    @IBOutlet weak var star05: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
     
+    private var stars: [UIButton] = []
     
     static func instantiateViewController() -> ReviewViewController {
         let storyboard = UIStoryboard(name: "Review", bundle: nil)
@@ -22,8 +29,7 @@ class ReviewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        stars = [star01, star02, star03, star04, star05]
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,6 +44,29 @@ extension ReviewViewController {
     
     @IBAction func didTapView(_ sender: UITapGestureRecognizer) {
         textView.resignFirstResponder()
+    }
+    
+    @IBAction func didTapStar(_ sender: UIButton) {
+        updateSendButton(isEnable: true)
+        evaluateStar(sender: sender)
+    }
+    
+    @IBAction func sendReview(_ sender: UIButton) {
+        
+    }
+    
+    private func evaluateStar(sender: UIButton) {
+        for star in stars {
+            star.backgroundColor = .white
+        }
+        for evaluate in 1...sender.tag {
+            stars[evaluate - 1].backgroundColor = .yellow
+        }
+    }
+    
+    func updateSendButton(isEnable: Bool) {
+        sendButton.isEnabled = isEnable
+        sendButton.alpha = (isEnable) ? 1 : 0.3
     }
     
 }
