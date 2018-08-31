72ef8c790518020a3ac33a56cb8bd48cde5e6780
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-31 10:48:13 +0900

    add User model

---
+++ nns.st/App/API/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  nns.st
+//
+//  Created by SHOHE on 2018/08/31.
+//  Copyright © 2018 OhtaniShohe. All rights reserved.
+//
+
+import Foundation
+
+struct User: Decodable {
+    let id: Int
+    let name: String
+    let email: String
+}
