c2d86fedd4a4235634c0e9decf7befcde0670ef5
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-31 10:28:07 +0900

    create EmailExistResponse class

---
+++ nns.st/App/API/Response/EmailExistResponse.swift
@@ -0,0 +1,20 @@
+//
+//  EmailExistResponse.swift
+//  nns.st
+//
+//  Created by SHOHE on 2018/08/31.
+//  Copyright © 2018 OhtaniShohe. All rights reserved.
+//
+
+import Foundation
+
+struct EmailExistResponse: Decodable {
+
+    let email: String
+    let isExisted: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        case email = "title"
+        case isExisted
+    }
+}
