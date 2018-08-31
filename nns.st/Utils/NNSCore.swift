9edc07f94c801d79beda05d6e32996961d528a36
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-31 10:48:28 +0900

    update NNSCore

--- nns.st/Utils/NNSCore.swift
+++ nns.st/Utils/NNSCore.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+enum NNSDataKey: String {
+    case authToken = "AuthToken"
+}
+
+class NNSCore {
+    class func authToken() -> String? {
+        return UserDefaults.standard.string(forKey: NNSDataKey.authToken.rawValue)
+    }
+    
+    class func setAuthToken(_ token: String?) {
+        if token == nil {return}
+        UserDefaults.standard.set(token!, forKey: NNSDataKey.authToken.rawValue)
+        UserDefaults.standard.synchronize()
+    }
+}
