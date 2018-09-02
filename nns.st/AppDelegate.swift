784ed170e062c01f65c10dfb511f1c63c68f9e3f
Shohe Ohtani <canada.shohe@gmail.com>
2018-09-02 16:11:36 +0900

    add requestDetailGetRequest

--- nns.st/AppDelegate.swift
+++ nns.st/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        API.requestDetailGetRequest(id: 1) { (result) in
+            if let res = result { print("res: \(res)") }
+        }
         return true
     }
 
