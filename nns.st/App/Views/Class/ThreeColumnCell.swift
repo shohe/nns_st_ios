b60a350723ddefafc275de16d05ff0e477ca2970
Shohe Ohtani <canada.shohe@gmail.com>
2018-09-06 00:20:18 +0900

    add element from IB

--- nns.st/App/Views/Class/ThreeColumnCell.swift
+++ nns.st/App/Views/Class/ThreeColumnCell.swift
@@ -22,6 +22,7 @@ class ThreeColumnCell: UICollectionViewCell {
         }
     }
 
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
